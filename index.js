bcc847101241ba7aff0728b5297d899858222a4e
Merajhosen <merajakhon7@gmail.com>
2022-04-26 23:50:01 +0600

    curd oparation server side

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const cors = require('cors');
+const { listen } = require('express/lib/application');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000
+const app = express()
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Running genius car server')
+})
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lopgm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db("geniusCar").collection("services");
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
+        // post
+        app.post('/services', async (req, res) => {
+            const newService = req.body
+            const result = await serviceCollection.insertOne(newService)
+            res.send(result)
+        })
+
+        //delete
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollection.deleteOne(query)
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+app.listen(port, () => {
+    console.log('listing to port', port)
+})
